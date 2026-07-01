785bc31e65fffbda47484f74daf2db660e374266
Ricardo Abreu <ricardo.abreu@canonical.com>
2026-07-01 19:12:13 +0100

    [sftp] Add a cooperative-stop facet to the session

--- src/ssh/plain_sftp_session.cpp
+++ src/ssh/plain_sftp_session.cpp
@@ -105,3 +105,8 @@ mp::PlainSftpSession::PlainSftpSession(PlainSSHSession&& ssh_session_obj,
                                              sshfs_process->borrow_channel(pass))}
 {
 }
+
+void mp::PlainSftpSession::request_stop()
+{
+    stop_requested.store(true);
+}
